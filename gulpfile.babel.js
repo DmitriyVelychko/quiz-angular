b72a23fde6f379d6f98f95dbaad2255eb9b5c030
Dmitriy Velychko <velychko.dmitriy@gmail.com>
2016-09-30 19:24:47 +0300

    Fixed comments gulpfile.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,17 +1,3 @@
-// const gulp = require('gulp');
-// const wrench = require('wrench');
-// const config = require('./gulp/config');
-// const gulpsync = require('gulp-sync')(gulp);
-// const bsReload = require('browser-sync').reload;
-//
-// wrench.readdirSyncRecursive('./gulp').filter(
-//   (file) => /\.(js)$/i
-//     .test(file))
-//     .map((file) => require(`./gulp/${file}`));
-//
-
-
-
 import config from './gulp/config';
 import Tasks from './gulp/tasks/task';
 
