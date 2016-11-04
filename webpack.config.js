87f8e83a45853d490475c7b8fa73d0622738bfa3
Dmitriy Velychko <dvelychko@sciforce.solutions>
2016-11-04 18:28:51 +0200

    Added environment property to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
+const webpack = require('webpack');
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 module.exports = {
   entry: './app/app.js',
+
   output: {
     path: './build/js',
     filename: 'bundle.js'
   },
+
   module: {
     loaders: [
       {
@@ -12,5 +18,30 @@ module.exports = {
         loader: 'babel-loader'
       }
     ]
-  }
+  },
+
+  plugins: [
+
+  ],
+
+  watch: NODE_ENV == 'development',
+
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+
+  devtool:  NODE_ENV == 'development' ? 'source-map' : null
+
 };
+
+if (NODE_ENV == 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: true,
+        unsafe: true
+      }
+    })
+  );
+}
